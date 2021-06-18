12be15f72795bf9611e554d81b16f8a51daa12fb
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-17 18:28:22 -0700

    lecture work-in-progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 
+// keys are the name we'll use to branch the state tree
+// for example: state.dragonList.members or state.appConfig.title
+
+// valuesa are the individual reducers, each of which manages its own portion of the complete state tree.
 
 export const rootReducer = combineReducers({
     dragonList: dragonListReducer,
-    title: titleReducer
+    appConfig: titleReducer
 }))
