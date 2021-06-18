bf2033ddbb6301b95f4b849d9142e6af0288be44
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-17 17:31:17 -0700

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 import Title from './components/Title';
+import { titleReducer } from './reducers/titleReducer';
 import './styles.css';
+
+// Step 1: create the store
+const store = createStore(titleReducer);
 
 function App() {
   return (
