fe3cd740eeffd01a6c49521453f86c0bf6ed8dbd
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-17 17:17:15 -0700

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Title from './components/Title';
-import DragonList from './components/DragonList';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <DragonList />
     </div>
   );
 }
