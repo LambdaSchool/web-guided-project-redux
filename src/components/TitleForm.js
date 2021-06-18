1af3ee20696d4f2371875f0051718473c8ab019f
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-17 17:58:42 -0700

    lecture work-in-progress

--- src/components/TitleForm.js
+++ src/components/TitleForm.js
@@ -26,4 +26,25 @@ const TitleForm = (props) => {
         </div>);
 }
 
-export default TitleForm;
+
+// Mapping functions for react-redux connect:
+
+// mapStateToProps to read state
+const mapStateToProps = (state) => {
+  return {
+    appName: state.appName,
+    editing: state.editing,
+    title: state.title
+  }
+}
+
+// mapDispatchToProps to update state
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleEditing: () => dispatch(toggleEditing()),
+    updateTitle: (title) => dispatch(updateTitle(title))
+  }
+}
+
+// Step 3: connect components to the Redux store (HOC pattern, "enhancing" the Title component with the ability to read from and update the Redux store)
+export default connect(mapStateToProps, mapDispatchToProps)(TitleForm);
