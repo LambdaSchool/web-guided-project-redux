fd49be64080b03204fb89bac62f2c16a1fbf7865
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-17 17:57:53 -0700

    lecture work-in-progress

--- src/components/TitleDisplay.js
+++ src/components/TitleDisplay.js
@@ -10,4 +10,25 @@ const TitleDisplay = (props)=> {
     </h2>);
 }
 
-export default TitleDisplay;
+
+// Mapping functions for react-redux connect:
+
+// mapStateToProps to read state
+const mapStateToProps = (state) => {
+  return {
+    appName: state.appName,
+    editing: state.editing,
+    title: state.title
+  }
+}
+
+// mapDispatchToProps to update state
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleEditing: () => dispatch(toggleEditing()),
+    updateTitle: (title) => dispatch(updateTitle(title))
+  }
+}
+
+// Step 3: connect components to the Redux store (HOC pattern, "enhancing" the Title component with the ability to read from and update the Redux store)
+export default connect(mapStateToProps, mapDispatchToProps)(TitleDisplay);
